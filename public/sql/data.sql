931a714dc1bdaa6226e69bf8385fa5e33e22eb40
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2025-02-03 15:03:45 +0300

    initialisation BDD

--- public/sql/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO variete_The (nom_variete, occupation, rendement, image_the)
-VALUES
-    ('Earl Grey', 2.5, 10.4, '/assets/images/18.jpg'),
-    ('Darjeeling', 3.0, 12.3, '/assets/images/18.jpg'),
-    ('Oolong', 1.8, 8.5, '/assets/images/18.jpg'),
-    ('Sencha', 2.2, 9.0, '/assets/images/18.jpg'),
-    ('Jasmine', 2.0, 7.8, '/assets/images/18.jpg');
