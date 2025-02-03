ebc7146727ad39427dae80709c5bc0bbfb19104a
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2025-02-03 15:42:32 +0300

    Update script.sql

--- public/sql/script.sql
+++ public/sql/script.sql
@@ -1,45 +1,39 @@
-CREATE TABLE Type_Alimentation(
-   id_type_alimentation INT AUTO_INCREMENT,
-   nom_type_alimentation INT NOT NULL,
-   PRIMARY KEY(id_type_alimentation)
+CREATE TABLE elevage_Type_Alimentation(
+   id_type_alimentation INTEGER AUTO_INCREMENT PRIMARY KEY,
+   nom_type_alimentation VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Alimentation(
-   id_alimentation INT AUTO_INCREMENT,
+CREATE TABLE elevage_Alimentation(
+   id_alimentation INTEGER AUTO_INCREMENT PRIMARY KEY,
    id_type_alimentation INT,
-   nom_aliment INT NOT NULL,
-   PRIMARY KEY(id_type_alimentation, id_alimentation),
-   FOREIGN KEY(id_type_alimentation) REFERENCES Type_Alimentation(id_type_alimentation)
+   nom_aliment VARCHAR(50) NOT NULL,
+   FOREIGN KEY(id_type_alimentation) REFERENCES elevage_Type_Alimentation(id_type_alimentation)
 );
 
-CREATE TABLE Utilisateur(
-   id_utilisateur INT AUTO_INCREMENT,
-   nom_utilsateur VARCHAR(50) NOT NULL,
-   argent DECIMAL(10,2),
-   PRIMARY KEY(id_utilisateur)
+CREATE TABLE elevage_Utilisateur(
+   id_utilisateur INTEGER AUTO_INCREMENT PRIMARY KEY,
+   nom_utilisateur VARCHAR(50) NOT NULL,
+   argent DECIMAL(10,2)
 );
 
-CREATE TABLE Type_Animal(
-   id_type_animal INT AUTO_INCREMENT,
+CREATE TABLE elevage_Type_Animal(
+   id_type_animal INTEGER AUTO_INCREMENT PRIMARY KEY,
+   nom_type VARCHAR(50) NOT NULL,
    poids_min_vente DECIMAL(10,2) NOT NULL,
-   prix_vente_kg VARCHAR(50) NOT NULL,
+   prix_vente_kg DECIMAL(10,2) NOT NULL,
    nb_jour_sans_manger INT,
    perte_poids DECIMAL(10,2) NOT NULL,
    id_type_alimentation INT,
-   nom_type VARCHAR(50) NOT NULL,
-   id_type_alimentation_1 INT NOT NULL,
-   PRIMARY KEY(id_type_animal),
-   FOREIGN KEY(id_type_alimentation) REFERENCES Type_Alimentation(id_type_alimentation)
+   FOREIGN KEY(id_type_alimentation) REFERENCES elevage_Type_Alimentation(id_type_alimentation),
 );
 
-CREATE TABLE Animal(
-   id_animal INT AUTO_INCREMENT,
+CREATE TABLE elevage_Animal(
+   id_animal INTEGER AUTO_INCREMENT PRIMARY KEY,
    id_utilisateur INT,
    id_type_animal INT,
    poids DECIMAL(10,2) NOT NULL,
-   image_animal VARCHAR(50) NOT NULL,
+   image_animal VARCHAR(255) NOT NULL,
    nom_animal VARCHAR(60),
-   PRIMARY KEY(id_animal),
-   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-   FOREIGN KEY(id_type_animal) REFERENCES Type_Animal(id_type_animal)
+   FOREIGN KEY(id_utilisateur) REFERENCES elevage_Utilisateur(id_utilisateur),
+   FOREIGN KEY(id_type_animal) REFERENCES elevage_Type_Animal(id_type_animal)
 );
