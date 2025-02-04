6dafd03357b0c92a4efe54bdc1b4d1efc9a414f2
Faniry_Ntsooa <faniry.rakotobe3@gmail.com>
2025-02-04 10:06:09 +0300

    date mort

--- app/controllers/MesAnimauxController.php
+++ app/controllers/MesAnimauxController.php
@@ -17,7 +17,16 @@ class MesAnimauxController {
         $animaux = Flight::MesAnimauxModel()-> getAllMesAnimaux();
         Flight::render('liste_mes_animaux', ['animaux' => $animaux]);
     }
+    public function ajouterDateMort() {
+        $id_animal = $_POST['id_animal'] ?? null;
+        $date_mort = $_POST['date_mort'] ?? null;
+
+        if ($id_animal && $date_mort) {
+            $this->model->updateDateMort($id_animal, $date_mort);
+            Flight::redirect(constant('BASE_URL').'liste/mesanimaux'); // Redirection après mise à jour
+        } else {
+            Flight::redirect(constant('BASE_URL').'liste/mesanimaux?error=missing_data');
+        }
+    }
     
-
-
 }
