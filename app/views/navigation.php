633ced7d4109964f182ae2fe5bac763b677002ed
Tsirisoadinah <tsirisoavalerie@gmail.com>
2025-02-04 13:09:46 +0300

    navbar

--- app/views/navigation.php
+++ app/views/navigation.php
@@ -4,18 +4,23 @@
         <div class="nav-align-top mb-4 w-100 d-flex justify-content-center">
             <ul class="nav nav-pills mb-3 nav-fill w-100" role="tablist">
                 <li class="nav-item">
-                    <a href="/admin/" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-add">
-                        <i class="tf-icons bx bx-plus"></i> Ajouter
+                    <a href="<?= constant('BASE_URL') ?>reset" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-add">
+                        <i class="tf-icons bx bx-bin"></i> Reset
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="/admin/" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-list">
-                        <i class="tf-icons bx bx-list-ul"></i> Liste
+                    <a href="<?= constant('BASE_URL') ?>prevision" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-list">
+                        <i class="tf-icons bx bx-list-ul"></i> Prevision
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="/admin/" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-config">
-                        <i class="tf-icons bx bx-cog"></i> Configuration
+                    <a href="<?= constant('BASE_URL') ?>depot" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-config">
+                        <i class="tf-icons bx bx-plus"></i> Depôt d'argent
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= constant('BASE_URL') ?>liste/alimentation" class="nav-link w-100" role="tab" aria-controls="navs-pills-justified-config">
+                        <i class="tf-icons bx bx-plus"></i> Achat Aliment
                     </a>
                 </li>
             </ul>
